3f2b85ab4690bedce7f8e8817960d7e59e84e50a
francesco scarniglia <francescoscarniglia@gmail.com>
2020-04-30 18:38:52 +0200

    ex 2 sent message

--- handLearbers/assets/js/main.js
+++ handLearbers/assets/js/main.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
-    console.log('jQuery è pronto');
+  // hendelbars chat
+
+  // ref
+  var input = $('.msg-input');
+  var button = $('.msg-btn');
+  var chat = $('.chat');
+
+  var source = $("#entry-template").html();
+  //console.log(source);
+  var template = Handlebars.compile(source);
+  //console.log(typeof source);
+  // console.log(typeof template);
+
+  // insert message
+  button.click(function() {
+    //console.log('hai pigiato su btn');
+    var message = input.val().trim();
+    // console.log(message);
+
+    // inserire in html
+    var data = {
+      text : message,
+      time : '18:30',
+      userClass : 'sent'
+    };
+
+    var html = template(data);
+    //console.log(html);
+
+    chat.append(html);
+
+    input.val('');
+
+  });
+
+
+
+
 });
